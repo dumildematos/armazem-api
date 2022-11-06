7ac650eda6611491658906aececb19c5caee166b
Dumilde Matos <dumildejosematos@gmail.com>
2022-11-06 18:52:12 +0000

    first commit

---
+++ src/main/java/com/carga/armazemapi/ArmazemApiApplication.java
@@ -0,0 +1,13 @@
+package com.carga.armazemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmazemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmazemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carga/armazemapi/ArmazemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carga.armazemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmazemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
